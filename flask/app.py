88d72fe7051459c7c59c734a903d869470af4496
francisco <pacocanham@live.co.uk>
2024-01-15 15:40:16 +0000

    added db and registration

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 from flask import Flask, redirect, render_template
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 import RPi.GPIO as GPIO
 from time import sleep
 import os
@@ -18,15 +21,29 @@ LR = GPIO.PWM(LRPIN, 50)
 UD.start(0)
 LR.start(0)
 
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 app = Flask(__name__)
+Session(app)
+db = SQL("sqlite:///babycam.db")
+
 
 @app.route("/")
 def index():
-#    (UDValue,LRValue,flipped) = loadconfig()
-#    return f"App Loaded<hr>LRPIN : {LRPIN}<br>UDPIN : {UDPIN}<br>UDValue : {UDValue}<br>LRValue : {LRValue}<br>Flipped : {flipped}<hr>"
-    return render_template("index.html")
+    (UDValue,LRValue,flipped) = loadconfig()
+    return f"App Loaded<hr>LRPIN : {LRPIN}<br>UDPIN : {UDPIN}<br>UDValue : {UDValue}<br>LRValue : {LRValue}<br>Flipped : {flipped}<hr>"
+#    return render_template("index.html")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 @app.route("/up")
+@login_required
 def up():
     (UDValue,LRValue,flipped) = loadconfig()
     UD.ChangeDutyCycle(UDValue) #to ensure movement
@@ -40,6 +57,7 @@ def up():
     return redirect("/")
     
 @app.route("/down")
+@login_required
 def down():
     (UDValue,LRValue,flipped) = loadconfig()
     UD.ChangeDutyCycle(UDValue) #to ensure movement
@@ -53,6 +71,7 @@ def down():
     return redirect("/")
 
 @app.route("/right")
+@login_required
 def right():
     (UDValue,LRValue,flipped) = loadconfig()
     LR.ChangeDutyCycle(LRValue) #to ensure movement
@@ -66,6 +85,7 @@ def right():
     return redirect("/")
 
 @app.route("/left")
+@login_required
 def left():
     (UDValue,LRValue,flipped) = loadconfig()
     LR.ChangeDutyCycle(LRValue) #to ensure movement
@@ -79,6 +99,7 @@ def left():
     return redirect("/")
 
 @app.route("/flip")
+@login_required
 def flip():
     (UDValue, LRValue, flipped) = loadconfig()
     if not flipped:
@@ -108,6 +129,58 @@ def saveconfig(UDValue, LRValue, flipped):
     with open ("config.json", "w") as config:
         data = {"UDValue" : UDValue,"LRValue" : LRValue,"flipped" : bool(flipped)}
         json.dump(data, config)
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/0.12/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    session.clear()
+    if request.method == "POST":
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username").lower())
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+        session["user_id"] = rows[0]["id"]
+        user_id = session["user_id"]
+        return redirect("/")
+    else:
+        return render_template("login.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username").lower()
+        passwordhash = generate_password_hash(request.form.get("password"))
+        usernamecheck = db.execute("SELECT 1 FROM users WHERE username = ?", username)
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("Passwords do not match")
+        if usernamecheck:
+            return apology("Username Taken")
+        elif not request.form.get("password"):
+            return apology("Please enter a password")
+        elif not request.form.get("username"):
+            return apology("Please enter a username")
+        else:
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, passwordhash)
+            session["user_id"] = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username").lower())[0]["id"]
+            user_id = session["user_id"]
+            return redirect("/")
+    else:
+        return render_template("register.html")
 
 if __name__ == '__main__':
     os.system('./mjpeg2.py &')
