798054c76cab93cc4eb5cdf9896ec8bf1542cfe7
francisco <pacocanham@live.co.uk>
2024-01-24 20:10:23 +0000

    pyaudio

--- flask/mjpeg2.py
+++ flask/mjpeg2.py
@@ -11,37 +11,6 @@ from libcamera import Transform
 from picamera2 import Picamera2
 from picamera2.encoders import MJPEGEncoder
 from picamera2.outputs import FileOutput
-import pyaudio
-import socket
-
-# Set up the PyAudio instance
-p = pyaudio.PyAudio()
-
-# Open the stream
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
-
-# Set up the socket
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(("0.0.0.0", 8087))
-s.listen(1)
-
-print("Waiting for a connection...")
-conn, addr = s.accept()
-print("Connected to: ", addr)
-
-try:
-    while True:
-        # Read data from the audio input stream
-        data = stream.read(1024)
-        
-        # Send data over the socket
-        conn.send(data)
-except KeyboardInterrupt:
-    # Stop the stream and close the socket when the script is interrupted
-    print("Interrupted... stopping stream and closing socket.")
-    stream.stop_stream()
-    stream.close()
-    conn.close()
 
 
 PAGE = """\
