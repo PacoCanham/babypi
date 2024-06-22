a37f88dd6a5bb61f2932a9a955b8a8d358246e25
Paco <francisco@technophobe.info>
2024-06-22 09:14:33 +0100

    fixed navbar

--- react/mantineVideo/src/Components/AppShell/Header/Header.jsx
+++ react/mantineVideo/src/Components/AppShell/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { Container, Flex, Grid, Group, Text, Title  } from "@mantine/core";
+import { Container, Flex, Grid, Group, SimpleGrid, Text, Title  } from "@mantine/core";
 import { useDisclosure, useSetState } from "@mantine/hooks";
 import { useEffect } from "react"
 import HeaderMenu from "./Segments/HeaderMenu";
@@ -11,14 +11,26 @@ export default function Header(props){
     
 
     return(
-        <Container fluid h={"100%"} bg="var(--mantine-color-blue-light)">
-        <Grid>
-            <Grid.Col span={9}><Group justify="Space-Between" align="Center" >
-                <Title order={1}>Hello {props.settings.username}</Title><Text onClick={()=>{props.setSettings({notifications:!props.settings.notifications})}}>
-            {(props.settings.notifications)?<IconBell/>:<IconBellOff/>}</Text></Group></Grid.Col>
-            <Grid.Col span={2} align="right"><Group justify="Space-between" align="center" ><Text size="xl" fw={500}>{props.settings.temp}°C </Text><Group align="center"><Text size='xl'>{props.settings.viewers}</Text><IconEye size={32}/></Group></Group></Grid.Col>
-            <Grid.Col span={1} align="left"><HeaderMenu displayed={props.displayed} setDisplayed={props.setDisplayed} settings={props.settings} setSettings={props.setSettings}/></Grid.Col>
-    </Grid>
+    <Container fluid h={"100%"} bg="var(--mantine-color-blue-light)">
+       <Flex
+      mih={50}
+      gap="md"
+      justify="space-between"
+      align="center"
+      direction="row"
+      wrap="nowrap">
+        <Group>
+            <Title flex="flex-start" order={1}>Hello {props.settings.username}</Title>
+        </Group>
+        <Group>
+            <Text onClick={()=>{props.setSettings({notifications:!props.settings.notifications})}}>
+            {(props.settings.notifications)?<IconBell/>:<IconBellOff/>}</Text>
+            <Text size="xl" fw={500}>{props.settings.temp}°C </Text>
+            <Text size='xl'>{props.settings.viewers}</Text>
+            <IconEye size={32}/>
+            <HeaderMenu displayed={props.displayed} setDisplayed={props.setDisplayed} settings={props.settings} setSettings={props.setSettings}/>
+        </Group>
+    </Flex>
     </Container>
     )
 }
