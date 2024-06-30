8aaaeb5d81d8b1654c32e39c8f29060b9b8defad
Paco <francisco@technophobe.info>
2024-06-30 16:12:57 +0100

    updated settings[audio] to settings[notifications][audio] etc

--- flask/modules/notification_settings.py
+++ flask/modules/notification_settings.py
@@ -8,25 +8,25 @@ from flask_session import Session
 @app.route("/getAudioConfig")
 @login_required
 def getAudioConf():
-    return jsonify(settings["audio"])
+    return jsonify(settings['notifications']["audio"])
 
 @app.route("/getVideoConfig")
 @login_required
 def getVideoConf():
-      return jsonify(settings["video"])
+      return jsonify(settings['notifications']["video"])
 
 @app.route("/applyVideo", methods=["POST"])
 @login_required
 def applyVideo():
     # data = request.json
-    settings['video'] = request.json 
+    settings['notifications']['video'] = request.json 
     print(settings)
     return "OK"
 
 @app.route("/applyAudio", methods=["POST"])
 @login_required
 def applyAudio():
-    settings['audio'] = request.json 
+    settings['notifications']['audio'] = request.json 
     print(settings)
     return "OK"
 
