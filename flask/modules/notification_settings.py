b2d97432540f1fa26a3f55651015ec79445d92ca
Paco <francisco@technophobe.info>
2024-06-30 16:09:52 +0100

    updated notification

--- flask/modules/notification_settings.py
+++ flask/modules/notification_settings.py
@@ -8,40 +8,12 @@ from flask_session import Session
 @app.route("/getAudioConfig")
 @login_required
 def getAudioConf():
-    return jsonify({'Vee': {
-        'delayLow': 600,
-        'delayHigh': 1800,
-        'volumeLow': 100,
-        'volumeHigh': 1000,
-        'sampleLength': 3,
-        'enabled' : True
-    },
-    'Paco': {
-        'delayLow': 600,
-        'delayHigh': 1800,
-        'volumeLow': 100,
-        'volumeHigh': 1000,
-        'sampleLength': 3,
-        'enabled' : True
-    }})
+    return jsonify(settings["audio"])
 
 @app.route("/getVideoConfig")
 @login_required
 def getVideoConf():
-      return {'Vee': {
-        'movThres': 60,
-        'movNumLow': 3,
-        'movNumHigh': 30,
-        'notificationDelay': 600,
-        'enabled' : True
-    },
-    'Paco': {
-        'movThres': 250,
-        'movNumLow': 3,
-        'movNumHigh': 30,
-        'notificationDelay': 600,
-        'enabled' : True
-    }}
+      return jsonify(settings["video"])
 
 @app.route("/applyVideo", methods=["POST"])
 @login_required
